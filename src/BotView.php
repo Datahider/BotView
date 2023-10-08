4751954bb13eecb6b6c45df12933a2b01ed09ad4
drweb <drweb@DESKTOP-6245KLE>
2023-10-08 11:18:54 +0300

    public API was documented

--- src/BotView.php
+++ src/BotView.php
@@ -20,13 +20,35 @@ class BotView {
     protected $chat_id;
     protected $api;
     
+    /**
+     * The constructor
+     * 
+     * @param BotApi $api           - an istance to TelegramBot\Api\BotApi
+     * @param int|string $chat_id   - a chat id to show data to
+     * @param type $template_suffix - a template suffix (optional, defaults to .php)
+     */
     public function __construct(BotApi $api, int|string $chat_id, $template_suffix='.php') {
         $this->api = $api;
         $this->chat_id = $chat_id;
         $this->template_suffix = $template_suffix;
     }
     
-    public function show(string $lang, string $message_template, string $keyboard_template=null, ?array $data=[], ?int $message_id=null) {
+    /**
+     * Displays a message to the chat. It can post a new message or edit 
+     * the existing one (if $message_id is given)
+     * 
+     * @param string $lang              - a language code
+     * @param string $message_template  - message template file name (will be appended by template suffix. See __constructor)
+     * @param string $keyboard_template - keyboard template file name (will be appended by template suffix. See __constructor)
+     *                                      The keyboard template must display a serialized instance of TelegramBot\Api\Types\Inline\InlineKeyboardMarkup 
+     * @param array|null $data          - data array to use in templates. Each key will be assigned as an internal template var
+     *                                      Ex. [ 'name' => 'sample', 'value' => 2 ] will assign internal template variables 
+     *                                      $name = 'sample'; $value = 2;
+     * @param int|null $message_id      - the id of the message to edit. If not given will post a new message
+     * 
+     * @return int                      - returns the id of posted or edited message
+     */
+    public function show(string $lang, string $message_template, string $keyboard_template=null, ?array $data=[], ?int $message_id=null) : int {
 
         $text = $this->processTemplate($lang, $message_template. $this->template_suffix, $data);
         
